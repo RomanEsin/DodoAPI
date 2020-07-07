d03225d11109cee453c63ef337737df7d3c619ca
Roman Esin <esinromanswift@gmail.com>
2020-07-08 01:30:42 +1000

    Fixed indentation.

--- index.js
+++ index.js
@@ -6,38 +6,38 @@ const url = 'https://dodopizza.ru/nakhodka/nahodkinskiy36';
 const http = require('http');
 
 function app(req, res) {
-  let date = new Date();
-  console.log(req.socket.remoteAddress, date);
-  rp(url)
-  .then(function(html){
-    //success!
-    const root = p.parse(html);
+    let date = new Date();
+    console.log(req.socket.remoteAddress, date);
+    rp(url)
+    .then(function(html){
+      //success!
+      const root = p.parse(html);
 
-    let names = root.querySelectorAll('.sc-1x0pa1d-1')
-    // console.log(names[0].childNodes[0].rawText);
-    let descriptions = root.querySelectorAll('.sc-1x0pa1d-0');
-    // console.log(descriptions[0].childNodes[2].rawText)
-    let prices = root.querySelectorAll('.money');
-    // console.log(prices[0].childNodes[0].childNodes[0].rawText);
-    let urls = root.querySelectorAll('.sc-1x0pa1d-6');
-    // console.log(base + urls[1].rawAttrs.split(' ')[0].split('=')[1].slice(1, -1).split('_')[3])
+      let names = root.querySelectorAll('.sc-1x0pa1d-1')
+      // console.log(names[0].childNodes[0].rawText);
+      let descriptions = root.querySelectorAll('.sc-1x0pa1d-0');
+      // console.log(descriptions[0].childNodes[2].rawText)
+      let prices = root.querySelectorAll('.money');
+      // console.log(prices[0].childNodes[0].childNodes[0].rawText);
+      let urls = root.querySelectorAll('.sc-1x0pa1d-6');
+      // console.log(base + urls[1].rawAttrs.split(' ')[0].split('=')[1].slice(1, -1).split('_')[3])
 
-    var data = {combos: [], other: []};
+      var data = {combos: [], other: []};
 
-    for (var i = 0; i < names.length; i++) {
-      if (urls[i].rawAttrs.split(' ')[0].split('=')[1].slice(1, -1).split('_')[3].length > 8) {
-        data.combos.push({name: names[i].childNodes[0].rawText, description: descriptions[i].childNodes[2].rawText, price: parseInt(prices[i].childNodes[0].childNodes[0].rawText)});
-      } else {
-        data.other.push({name: names[i].childNodes[0].rawText, description: descriptions[i].childNodes[2].rawText, price: parseInt(prices[i].childNodes[0].childNodes[0].rawText)}); 
-      }
+      for (var i = 0; i < names.length; i++) {
+        if (urls[i].rawAttrs.split(' ')[0].split('=')[1].slice(1, -1).split('_')[3].length > 8) {
+          data.combos.push({name: names[i].childNodes[0].rawText, description: descriptions[i].childNodes[2].rawText, price: parseInt(prices[i].childNodes[0].childNodes[0].rawText)});
+        } else {
+          data.other.push({name: names[i].childNodes[0].rawText, description: descriptions[i].childNodes[2].rawText, price: parseInt(prices[i].childNodes[0].childNodes[0].rawText)}); 
+        }
     }
 
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end(JSON.stringify(data));
+      res.writeHead(200, {'Content-Type': 'text/plain'});
+      res.end(JSON.stringify(data));
   })
   .catch(function(err){
-    //handle error
-    console.log(err);
+      //handle error
+      console.log(err);
   });
 }
 
